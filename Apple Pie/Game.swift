d4189ff29cf611affb0bb32fab5c0f32cb65c891
Frank Solleveld <solleveld@coffeeit.nl>
2019-10-24 08:52:52 +0200

    initial commit

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Frank Solleveld on 24/10/2019.
+//  Copyright © 2019 Frank Solleveld. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    // MARK: Variables
+    var word: String
+    var incorrectMovesRemaining: Int
+
+    // MARK: Functions
+       
+}
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Variables
+    var listOfWords: [String] = ["spaghetti", "bolognese", "apple", "samsung", "cowboy", "pleb", "iphone", "swift", "twostraws"]
+    var incorrectMovesAllowed: Int = 7
+    var totalWins: Int = 0
+    var totalLosses: Int = 0
+    var currentGame: Game!
+    
+    // MARK: Outlets
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        newRound()
+    }
+    
+    // MARK: Actions
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
     }
 
-
+    // MARK: Functions
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
+    }
+    
+    func updateUI() {
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
+ 
 }
 
